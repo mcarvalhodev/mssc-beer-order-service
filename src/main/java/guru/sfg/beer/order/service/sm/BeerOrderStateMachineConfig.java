b03b3d81bfb84d0b2e799c45be91a81f40284ba3
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-06-13 19:23:45 -0300

    Configuring SM transitions for validation

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -6,24 +6,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
 @Configuration
 @EnableStateMachineFactory
 public class BeerOrderStateMachineConfig
-        extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+    extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
 
   @Override
   public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states)
-          throws Exception {
+      throws Exception {
     states
-            .withStates()
-            .initial(BeerOrderStatusEnum.NEW)
-            .states(EnumSet.allOf(BeerOrderStatusEnum.class))
-            .end(BeerOrderStatusEnum.DELIVERED)
-            .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
-            .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
-            .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+        .withStates()
+        .initial(BeerOrderStatusEnum.NEW)
+        .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+        .end(BeerOrderStatusEnum.DELIVERED)
+        .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+        .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+        .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+  }
+
+  @Override
+  public void configure(
+      StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions)
+      throws Exception {
+    transitions
+        .withExternal()
+        .source(BeerOrderStatusEnum.NEW)
+        .target(BeerOrderStatusEnum.NEW)
+        .event(BeerOrderEventEnum.VALIDATE_ORDER)
+        .and()
+        .withExternal()
+        .source(BeerOrderStatusEnum.NEW)
+        .target(BeerOrderStatusEnum.VALIDATED)
+        .event(BeerOrderEventEnum.VALIDATION_PASSED)
+        .and()
+        .withExternal()
+        .source(BeerOrderStatusEnum.NEW)
+        .target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+        .event(BeerOrderEventEnum.VALIDATION_FAILED);
   }
 }
